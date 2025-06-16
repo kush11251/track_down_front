19abbb09f6a1fa2460b700cae26ce708d9064486
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2025-06-17 01:20:06 +0530

    Added Documentation doc

--- src/app/common-screens/documentation/documentation.component.ts
+++ src/app/common-screens/documentation/documentation.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './documentation.component.css'
 })
 export class DocumentationComponent {
-
+  pdfUrl = 'https://ucarecdn.com/5a30c087-9655-4004-8563-d0dacf11cfa6/'
 }
