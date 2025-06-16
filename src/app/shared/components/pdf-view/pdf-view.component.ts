af8fa8982ff2262bf937d6c77940a3e107eb54df
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2025-06-17 02:19:39 +0530

    PDF Component updated for safari

--- src/app/shared/components/pdf-view/pdf-view.component.ts
+++ src/app/shared/components/pdf-view/pdf-view.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input, ElementRef, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { SafeUrlPipe } from "../../../pipe/safe-url.pipe";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pdf-view',
   standalone: true,
-  imports: [SafeUrlPipe],
+  imports: [SafeUrlPipe, CommonModule],
   templateUrl: './pdf-view.component.html',
   styleUrl: './pdf-view.component.css'
 })
 export class PdfViewComponent implements OnInit{
   @Input() pdfUrl: string = '';
+
+  isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
   ngOnInit(): void {
     console.log("Pdf url: ", this.pdfUrl)
