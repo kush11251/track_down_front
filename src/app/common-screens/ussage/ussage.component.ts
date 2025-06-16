94a4a949f59a55f66893c4ded5e05d3e93dc5405
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2025-06-17 00:30:19 +0530

    Added ussage doc

--- src/app/common-screens/ussage/ussage.component.ts
+++ src/app/common-screens/ussage/ussage.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './ussage.component.css'
 })
 export class UssageComponent {
-
+  pdfUrl = 'https://ucarecdn.com/e4056500-14b8-49f7-aec2-204f8501a03f/'
 }
